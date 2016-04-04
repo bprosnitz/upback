f8f7d8a405deef5d999acb262b2a5dfb376d226d
Benjamin Prosnitz <bprosnitz@google.com>
2016-04-03 22:53:30 -0700

    Update mock filesystem

--- go/src/drivebackup/store/filesystem/mock/mock_filesystem.go
+++ go/src/drivebackup/store/filesystem/mock/mock_filesystem.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"os"
 	"time"
+	"path/filepath"
 )
 
 type MockFilesystemService struct {
@@ -16,124 +17,171 @@ func (m *MockFilesystemService) Bucket(bucket string) filesystem.Bucket {
 	if b, ok := m.m[bucket]; ok {
 		return b
 	}
-	b := &mockBucket{fileVersions: map[string]*mockFileVersions{}}
+	b := &mockBucket{fileVersions: map[string]*mockFile{}}
 	m.m[bucket] = b
 	return b
 }
 
-// a particular version of a file
-type mockFileEntry struct {
-	blobRef filesystem.BlobRef
-	version string
+// represents all versions of a particular file
+type mockFile struct {
+	entries []*filesystem.StoredBlobRef
 }
 
-// represents all versions of a particular file
-type mockFileVersions struct {
-	entries []*mockFileEntry
+type mockDir struct {
+	versions []*filesystem.Version
 }
 
 type mockBucket struct {
-	fileVersions map[string]*mockFileVersions
+	fileVersions map[string]*mockFile
+	dirVersions map[string]*mockDir
 	latestVersion string
-}
-
-func (m *mockBucket) NewPutTransaction() filesystem.PutTransaction {
-	return &mockPutTransaction{bucket: m}
-}
-func (m *mockBucket) Commit(tx filesystem.PutTransaction) error {
-	mtx, ok := tx.(*mockPutTransaction)
-	if !ok {
-		return fmt.Errorf("only can commit *MockPutTransaction")
-	}
-	version := fmt.Sprintf("%d", time.Now().Unix())
-	for version == m.latestVersion {
-		<- time.After(100 * time.Millisecond)
-		version = fmt.Sprintf("%d", time.Now().Unix())
-	}
-	for name, ref := range mtx.blobs {
-		cell, ok := m.fileVersions[name]
-		if !ok {
-			cell = &mockFileVersions{}
-			m.fileVersions[name] = cell
-		}
-		cell.entries = append(cell.entries, &mockFileEntry{ref, version})
-	}
-	m.latestVersion = version
-	return nil
-}
-func (m *mockBucket) VersionsForPath(path string) ([]filesystem.FilesystemVersion, error) {
-	var fsv []filesystem.FilesystemVersion
-	if versions, ok := m.fileVersions[path]; ok {
-		for _, entry := range versions.entries {
-			fsv = append(&mockFilesystemVersion{entry.version, m})
-		}
-	}
-	return fsv, nil
-}
-func (m *mockBucket) LatestFile(path string) (filesystem.BlobRef, error) {
-	if versions, ok := m.fileVersions[path]; ok {
-		var maxKey string
-		for _, entry := range versions.entries {
-			if entry.version > maxKey {
-				maxKey = entry.version
-			}
-		}
-		for _, entry := range versions.entries {
-			if entry.version == maxKey {
-				return entry.blobRef, nil
-			}
-		}
-		}
-	return nil, nil
-}
-func (m *mockBucket) Latest() (filesystem.FilesystemVersion, error) {
-	if m.latestVersion == "" {
-		return nil, nil
-	}
-	return &mockFilesystemVersion{m.latestVersion, m}, nil
 }
 
 // put transaction
 type mockPutTransaction struct {
 	blobs map[string]filesystem.BlobRef
+	dirs map[string]bool
+	bucket *mockBucket
+	currPath string
+}
+
+type mockConstraintType int
+
+const (
+	mockVersionConstraint mockConstraintType = iota
+	mockLatestConstraint
+	mockDirConstraint
+	mockFileConstraint
+)
+
+type mockConstraint struct {
+	t mockConstraintType
+	version filesystem.Version // if version constraint
+	path string // if dir/file constraint
+}
+
+type mockSelector struct {
+	constraints []mockConstraint
 	bucket *mockBucket
 }
 
-// view into the filesystem for a specific version
-type mockFilesystemVersion struct {
-	version string
-	bucket *mockBucket
+func (m *mockBucket) NewPutTransaction() filesystem.PutTransaction {
+	return &mockPutTransaction{bucket: m}
+}
+func (m *mockBucket) Select() filesystem.Selector {
+	return &mockSelector{bucket: m}
 }
 
-func (m *mockFilesystemVersion) entry(path string) *mockFileEntry {
-	if file, ok := m.bucket.fileVersions[path]; ok {
-		for _, entry := range file.entries {
-			if entry.version == m.version {
-				return entry
+func (tx *mockPutTransaction) Commit() error {
+	version := fmt.Sprintf("%d", time.Now().Unix())
+	for version == tx.bucket.latestVersion {
+		<- time.After(100 * time.Millisecond)
+		version = fmt.Sprintf("%d", time.Now().Unix())
+	}
+	for path := range tx.dirs {
+		dir, ok := tx.bucket.dirVersions[path]
+		if !ok {
+			dir = &mockDir{}
+			tx.bucket.dirVersions[path] = dir
+		}
+		dir.versions = append(dir.versions, version)
+	}
+	for name, ref := range tx.blobs {
+		cell, ok := tx.bucket.fileVersions[name]
+		if !ok {
+			cell = &mockFile{}
+			tx.bucket.fileVersions[name] = cell
+		}
+		cell.entries = append(cell.entries, &filesystem.StoredBlobRef{ref, version})
+	}
+	tx.bucket.latestVersion = version
+	return nil
+}
+
+func (tx *mockPutTransaction) Dir(path string) filesystem.PutTransactionPath {
+	fullPath := filepath.Join(tx.currPath, path)
+	tx.dirs[fullPath] = true
+	return &mockPutTransaction{blobs: tx.blobs, dirs: tx.dirs, bucket: tx.bucket, path: fullPath}
+}
+
+func (tx *mockPutTransaction) File(name string, blobRef filesystem.BlobRef) {
+	tx.blobs[filepath.Join(tx.currPath, name)] = blobRef
+}
+
+func (s *mockSelector) Version(version filesystem.Version) filesystem.Selector {
+	return &mockSelector{
+		constraints: append(s.constraints, &mockConstraint{t: mockVersionConstraint, version: version}),
+		bucket: s.bucket,
+	}
+}
+func (s *mockSelector) Latest() filesystem.Selector {
+	return &mockSelector{
+		constraints: append(s.constraints, &mockConstraint{t: mockLatestConstraint}),
+		bucket: s.bucket,
+	}
+}
+func (s *mockSelector) Dir(path string) filesystem.Selector {
+	return &mockSelector{
+		constraints: append(s.constraints, &mockConstraint{t: mockDirConstraint, path: path}),
+		bucket: s.bucket,
+	}
+}
+func (s *mockSelector) File(name string) filesystem.Selector {
+	return &mockSelector{
+		constraints: append(s.constraints, &mockConstraint{t: mockFileConstraint, path: name}),
+		bucket: s.bucket,
+	}
+}
+func (s *mockSelector) validate(fileOp bool) (err error) {
+	var sawVersionConstraint bool
+	firstFileIndex := -1
+	for i, constraint := range s.constraints {
+		switch constraint.t {
+		case mockVersionConstraint, mockLatestConstraint:
+			if sawVersionConstraint {
+				return false, fmt.Errorf("Version()/Latest() may only be called once")
 			}
+			sawVersionConstraint = true
+		case mockFileConstraint:
+			if firstFileIndex != -1 {
+				return false, fmt.Errorf("File() must not be called twice")
+			}
+			firstFileIndex = i
+		}
+	}
+	if firstFileIndex < len(s.constraints) - 2 {
+		return fmt.Errorf("File() must be the last filesystem selector")
+	} else if s.constraints[len(s.constraints)-1].t == mockDirConstraint {
+		return fmt.Errorf("File() must be the last filesystem selector")
+	}
+	if fileOp {
+		if firstFileIndex == -1 {
+			return fmt.Errorf("No File() selector specified for file operation")
+		}
+	} else {
+		if firstFileIndex != -1 {
+			return fmt.Errorf("File() selector incorrectly specified for directory operation")
 		}
 	}
 	return nil
 }
-
-func (m *mockFilesystemVersion) ListDir(path string) ([]string, error) {
-	if path[len(path)-1] != os.PathSeparator {
-		path = append(path, os.PathSeparator)
+func (s *mockSelector) List() ([]string, error) {
+	if err := s.validate(false); err != nil {
+		return nil, err
 	}
-
-	files := map[string]bool{}
-	for filePath, _ := range m.bucket.fileVersions {
-		if strings.HasPrefix(filePath, path) {
-			files[path + strings.Split(strings.TrimPrefix(filePath, path),""+os.PathSeparator)[0]] = true
-		}
-	}
-	return files, nil
+	// DOSTUFF
 }
-
-func (m *mockFilesystemVersion) GetFile(path string) (filesystem.BlobRef, error) {
-	entry := m.entry(path)
-	if entry != nil {
-		return entry.blobRef, nil
+func (s *mockSelector) Ref() (filesystem.StoredBlobRef, error) {
+	if err := s.validate(true); err != nil {
+		return nil, err
 	}
-	return nil, nil
+	// DOSTUFF
+
+}
+func (s *mockSelector) Versions() ([]filesystem.StoredBlobRef, error) {
+	if err := s.validate(true); err != nil {
+		return nil, err
+	}
+	// DOSTUFF
+
 }
